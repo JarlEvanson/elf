747b6ca3dc13852a12377753b4ea3eeb5e23f08c
Jarl Evanson <evanson.jarl@gmail.com>
2024-10-13 21:42:54 -0500

    Add `ClassParse`, `Class32, `Class64`, and `Merge`

--- src/class/mod.rs
+++ src/class/mod.rs
@@ -4,12 +4,27 @@ use core::{error, fmt};
 
 use crate::{encoding::EncodingParse, ident::Class};
 
+mod class_32;
+pub use class_32::*;
+
+mod class_64;
+pub use class_64::*;
+
+mod merge;
+pub use merge::*;
+
+/// An object offering methods for parsing both 32-bit and 64-bit ELF files.
+pub type AnyClass = Merge<Class32, Class64>;
+
+/// A combination of all other class parsing traits.
+pub trait ClassParse: ClassParseBase {}
+
 /// The base definitions of a class aware parser.
 pub trait ClassParseBase: Clone + Copy {
     /// An unsigned class sized integer.
-    type ClassUsize: TryInto<usize>;
+    type ClassUsize: TryInto<usize> + fmt::Debug + fmt::Display;
     /// A signed class sized integer.
-    type ClassIsize;
+    type ClassIsize: fmt::Debug + fmt::Display;
 
     /// Returns the [`ClassParseBase`] instance that corresponds with the given [`Class`].
     ///
